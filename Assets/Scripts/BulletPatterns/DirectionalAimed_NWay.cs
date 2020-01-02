deb6646ff1a8cd58ab99cd8e84e538254eec3a1c
윤용운_Game <yuyun0124@gmail.com>
2020-01-02 22:07:24 +0900

    Add DirectionalAimed_NWay

--- Assets/Scripts/BulletPatterns/DirectionalAimed_NWay.cs
+++ Assets/Scripts/BulletPatterns/DirectionalAimed_NWay.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirectionalAimedRandom : AimedBulletPattern
+public class DirectionalAimed_NWay : AimedBulletPattern
 {
     [Header("※ 본 패턴은 Bullet의 속도를 덮어씁니다.")]
-    public int count = 10;
-    public float minSpeed = 4f;
-    public float maxSpeed = 7f;
-    public float inDelay = 0.05f;
+    public int count = 3;
+    public float Speed = 4f;
     public float outDelay = 0.5f;
     public float angleRange = 0.1f;
 
     private Vector3 m_targetPos;
-    private float m_dx;
-    private float m_dy;
     private float m_angleRange;
+    private float m_angleAlpha;
     private float m_speed;
 
     protected override IEnumerator Fire()
@@ -26,12 +23,14 @@ public class DirectionalAimedRandom : AimedBulletPattern
             Vector2 direction = target.position - transform.position;
             float atan = Mathf.Atan(direction.y / direction.x);
 
+            m_angleAlpha = 2 * angleRange / (count - 1);
+            m_angleRange = angleRange;
+
             GetComponentInParent<AudioSource>().PlayOneShot(audioclip);
 
             for (int i = 0; i < count; i++)
             {
-                m_angleRange = Random.Range(-angleRange, angleRange);
-                m_speed = Random.Range(minSpeed, maxSpeed);
+                m_speed = Speed;
                 GameObject bulletInst = PoolManager.instance.PopFromPool(bullet.name);
                 bulletInst.transform.position = transform.position;
                 bulletInst.SetActive(true);
@@ -41,11 +40,11 @@ public class DirectionalAimedRandom : AimedBulletPattern
                 else  // 플레이어가 오른쪽
                     bulletInst.GetComponent<Rigidbody2D>().velocity = new Vector2(m_speed * Mathf.Cos(atan + m_angleRange), m_speed * Mathf.Sin(atan + m_angleRange));
 
-                yield return new WaitForSeconds(inDelay);
+                m_angleRange -= m_angleAlpha;
             }
 
             yield return new WaitForSeconds(outDelay);
+
         }
     }
 }
-
