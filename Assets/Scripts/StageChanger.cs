76450ba4ed3fe49639c559916a016c4724600b9c
irikong <gookirix@gmail.com>
2020-01-04 17:24:23 +0900

    Add stage changer

---
+++ Assets/Scripts/StageChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageChanger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController.instance.moveNextStage = true;
+            Invoke("LoadNextStage", 2f);
+        }
+    }
+
+    private void LoadNextStage()
+    {
+        PlayerController.instance.moveNextStage = false;
+        SceneManager.LoadScene("Menu");
+    }
+}
