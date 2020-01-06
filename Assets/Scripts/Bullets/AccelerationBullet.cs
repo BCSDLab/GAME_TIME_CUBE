67fb18c412e5bab535a8247ae01f6975890e3e6c
YJ CHO <reverince@gmail.com>
2020-01-06 12:32:42 +0900

    Add min/max speed to AccelerationBullet

--- Assets/Scripts/Bullets/AccelerationBullet.cs
+++ Assets/Scripts/Bullets/AccelerationBullet.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class AccelerationBullet : Bullet
 {
     [SerializeField]
-    private float m_accelRate = 1.0f;
+    private float m_accelRate = 0.5f;
+    [SerializeField]
+    private bool m_useMaxSpeed = true;
+    [SerializeField]
+    private float m_maxSpeed = 10;
+    [SerializeField]
+    private bool m_useMinSpeed = true;
+    [SerializeField]
+    private float m_minSpeed = 1f;
 
     private Rigidbody2D m_rigidbody;
 
@@ -16,6 +24,9 @@ public class AccelerationBullet : Bullet
 
     void FixedUpdate()
     {
+        Debug.Log(m_rigidbody.velocity.magnitude);
+        if (m_useMaxSpeed && m_rigidbody.velocity.magnitude >= m_maxSpeed) return;
+        if (m_useMinSpeed && m_rigidbody.velocity.magnitude <= m_minSpeed) return;
         Vector2 newVelocity = m_rigidbody.velocity * (m_accelRate * Time.deltaTime);
         m_rigidbody.velocity += newVelocity;
     }
