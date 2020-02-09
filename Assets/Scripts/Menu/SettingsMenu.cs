4d0a00783a1d99eed392de44a62dcba3f6f41440
YJ CHO <reverince@gmail.com>
2020-02-09 16:56:26 +0900

    Display frameRate on SettingsMenu

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -52,7 +52,7 @@ public class SettingsMenu : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " × " + resolutions[i].height;
+            string option = resolutions[i].width + " × " + resolutions[i].height + " (" + resolutions[i].refreshRate + "Hz)";
             options.Add(option);
 
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
