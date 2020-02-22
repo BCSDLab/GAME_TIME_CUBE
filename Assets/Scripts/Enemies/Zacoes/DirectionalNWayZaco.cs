0717522042b3aeee2d02427d8d775f88def0e1aa
윤용운_Game <yuyun0124@gmail.com>
2020-02-22 15:32:07 +0900

    Add Stage1 Zaco #3

--- Assets/Scripts/Enemies/Zacoes/DirectionalNWayZaco.cs
+++ Assets/Scripts/Enemies/Zacoes/DirectionalNWayZaco.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DirectionalNWayZaco : ShootingZaco
 {
+    private Cross m_cross;
     protected override void Start()
     {
         base.Start();
         m_bulletPattern = GetComponent<DirectionalAimedNWay>();
+        m_cross = GetComponent<Cross>();
     }
 }
