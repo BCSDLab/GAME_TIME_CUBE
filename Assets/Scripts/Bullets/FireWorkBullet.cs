22e0ed554fd169cf847a5e68118c34df0f021dd6
윤용운_Game <yuyun0124@gmail.com>
2020-03-06 01:38:10 +0900

    Add Stage3 Zaco #1(Add Firework Pattern) / ~37s

---
+++ Assets/Scripts/Bullets/FireWorkBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DirectionalUp))]
+public class FireWorkBullet : Bullet
+{
+    private BulletPattern m_bulletPattern;
+    private float m_time = 0f;
+    [SerializeField]
+    private float m_crackTime = 3f;
+
+    void Awake()
+    {
+        m_bulletPattern = GetComponent<RadialMulti>();
+    }
+
+    void OnEnable()
+    {
+        m_time = 0f;
+    }
+
+    void Update()
+    {
+        m_time += Time.deltaTime;
+
+        if (m_crackTime < m_time)
+        {
+            StartCoroutine("BulletDestroy");
+        }
+    }
+
+    IEnumerator BulletDestroy()
+    {
+        m_bulletPattern.StartPattern();
+        yield return new WaitForSeconds(0.01f);
+        PoolManager.instance.PushToPool(gameObject);
+    }
+}
