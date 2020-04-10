77a3111ef3a8924a3925b9faee2b1358fa7f49fc
윤용운_Game <yuyun0124@gmail.com>
2020-04-10 20:06:24 +0900

    Complete Boss2 Sound

--- Assets/Scripts/Patterns/Cross.cs
+++ Assets/Scripts/Patterns/Cross.cs
@@ -8,6 +8,9 @@ public class Cross : BulletPattern
     public float speed;
     public float delay = 1f;
 
+    [SerializeField]
+    private bool m_hasDestroyedSound = false;
+
     protected override IEnumerator Fire()
     {
         float theta = 360f / count;
@@ -19,6 +22,11 @@ public class Cross : BulletPattern
             bulletInst.transform.position = m_spawnPos.position;
             bulletInst.transform.rotation = Quaternion.Euler(Vector3.forward * (theta * i));
             bulletInst.SetActive(true);
+
+            if (m_hasDestroyedSound && i == 0)
+            {
+                bulletInst.GetComponent<AudioSource>().PlayOneShot(m_ShotSFX, m_ShotSFXVolum/8f);
+            }
 
             bulletInst.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Mathf.Cos(Mathf.PI * 2f * i / count), speed * Mathf.Sin(Mathf.PI * 2f * i / count));
         }
